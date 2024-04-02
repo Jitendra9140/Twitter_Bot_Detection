f054f5dae3a7cf62866513f72e480766296646a1
LokeshPatil-loki <lokesh1404@outlook.com>
2024-04-02 22:31:29 +0530

    Fixed Column names and preprocess_input function

--- application.py
+++ application.py
@@ -5,41 +5,53 @@ import pandas as pd
 import numpy as np
 import time
 from datetime import datetime
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import hstack
 
 app = Flask(__name__)
 CORS(app)
 model = pickle.load(open('model.pkl', 'rb'))
 tweeter = pd.read_csv('bot_detection_data.csv')
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
-# Define a function to preprocess the input data
+
 def preprocess_input(username, tweet, hashtag, location, created_at, retweet_count, mention_count, follower_count, verified_status):
-    # Convert created_at to Unix timestamp
-    created_at_datetime = datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S')
-    created_at_unix = int(time.mktime(created_at_datetime.timetuple()))
-    
+
     # Prepare input data as DataFrame
     input_data = pd.DataFrame({
-        'username': [username],
-        'tweet': [tweet],
-        'location': [location],
-        'hashtag': [hashtag],
-        'retweet_count': [retweet_count],
-        'verified_status': [verified_status],
-        'mention_count': [mention_count],
-        'follower_count': [follower_count],
-        'created_at_unix': [created_at_unix]
+        'Username': [username],
+        'Tweet': [tweet],
+        'Location': [location],
+        'Hashtags': [hashtag],
+        'Retweet Count': [retweet_count],
+        'Verified': [verified_status],
+        'Mention Count': [mention_count],
+        'Follower Count': [follower_count],
+        'Created At': [created_at]
     })
 
-    return input_data
+    new_text_data = input_data['Tweet'] + ' ' + input_data['Username'] + \
+        ' ' + input_data['Hashtags']+' '+input_data['Location']
 
-# Define a function to make predictions
+    new_text_sparse = vectorizer.transform(new_text_data)
+
+    new_additional_features = input_data[[
+        'Retweet Count', 'Verified', 'Mention Count', 'Follower Count', 'Created At']]
+    new_additional_features = new_additional_features.astype('float64')
+    new_combined_sparse = hstack((new_text_sparse, new_additional_features))
+
+    return new_combined_sparse
+
+
 def make_prediction(input_data):
     prediction = model.predict(input_data)
     return prediction
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -49,20 +61,20 @@ def predict():
     hashtag = request.form.get('Hashtag')
     location = request.form.get('location')
     created_at = request.form.get('created')
-    retweet_count = int(request.form.get('rtweet', 0))  # Default to 0 if not provided
-    mention_count = int(request.form.get('mcount', 0))  # Default to 0 if not provided
-    follower_count = int(request.form.get('fcount', 0))  # Default to 0 if not provided
-    verified_status = bool(request.form.get('verified', False))  # Convert to boolean
+    # Default to 0 if not provided
+    retweet_count = int(request.form.get('rtweet', 0))
+    mention_count = int(request.form.get('mcount', 0))
+    follower_count = int(request.form.get('fcount', 0))
+    verified_status = bool(request.form.get(
+        'verified', False))  # Convert to boolean
 
-    # Preprocess input data
-    input_data = preprocess_input(username, tweet, hashtag, location, created_at, retweet_count, mention_count, follower_count, verified_status)
+    input_data = preprocess_input(username, tweet, hashtag, location, created_at,
+                                  retweet_count, mention_count, follower_count, verified_status)
 
-    # Make prediction
     prediction = make_prediction(input_data)
 
-    # Return the prediction
-    return jsonify({'prediction': prediction})
+    return "BOT" if prediction else "Not Bot"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
